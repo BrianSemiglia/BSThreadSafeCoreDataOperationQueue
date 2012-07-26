1cd14da9e297a39b25cfeaf5ce1bdc8c9f0a51ec
Brian Semiglia <brian.semiglia@gmail.com>
2012-07-26 12:06:03 -0700

    ReadME update.

--- BSConcurrentManagedObjectContext.h
+++ BSConcurrentManagedObjectContext.h
@@ -1,5 +1,5 @@
 //
-//  MFConcurrentManagedObjectContext.h
+//  BSConcurrentManagedObjectContext.h
 //
 //
 //  Created by Brian on 7/24/12.
@@ -7,6 +7,17 @@
 //
 
 #import <CoreData/CoreData.h>
+
+// The first time a BSConcurrentManagedObjectContext is created, a singleton parent context is also
+// created. From then on any BSConcurrentManagedObjectContext's created save to that parent context.
+// The singleton parent context runs on a private serial queue in the background. This removes the need for
+// a NSManagedObjectContext within your application delegate. A BSConcurrentManagedObjectContext has the
+// components for assigning it perisistentStoreCoordinator within it. This was done for convenience and to
+// ensure that the child and parent contexts are on the same page.
+
+// All saves and fetches are executed asynchronously from the main thread but dispatched to a serial queue,
+// ensuring that saves happen in the order that they were submitted. The completion handler is run on the
+// thread that it's method was called on.
 
 @interface BSConcurrentManagedObjectContext : NSManagedObjectContext
 
